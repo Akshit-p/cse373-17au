740d3a1047d81e7491100a67e6a803f36c6e6d90
Akshit Patel <akshitpatel.cs@gmail.com>
2017-09-30 02:50:31 -0700

    complete partB may need comments and efficiency and style improvements

--- project1/src/main/java/datastructures/concrete/DoubleLinkedList.java
+++ project1/src/main/java/datastructures/concrete/DoubleLinkedList.java
@@ -8,8 +8,7 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
- * Note: For more info on the expected behavior of your methods, see
- * the source code for IList.
+ * Note: For more info on the expected behavior of your methods, see the source code for IList.
  */
 public class DoubleLinkedList<T> implements IList<T> {
     // You may not rename these fields or change their types.
@@ -27,47 +26,188 @@ public class DoubleLinkedList<T> implements IList<T> {
 
     @Override
     public void add(T item) {
-        throw new NotYetImplementedException();
+        if (this.size == 0) {
+            this.front = new Node<T>(item);
+            this.back = this.front;
+        } else {
+            this.back = new Node<T>(this.back, item, null);
+            this.back.prev.next = this.back;
+        }
+        this.size++;
     }
 
     @Override
     public T remove() {
-        throw new NotYetImplementedException();
+        if (this.isEmpty()) {
+            throw new EmptyContainerException();
+        }
+        T data;
+        if (this.size == 1) {
+            data = this.front.data;
+            this.front = null;
+            this.back = null;
+        } else {
+            data = this.back.data;
+            this.back = this.back.prev;
+            this.back.next = null;
+        }
+        size--;
+        return data;
     }
 
     @Override
     public T get(int index) {
-        throw new NotYetImplementedException();
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return getNode(index).data;
     }
 
     @Override
     public void set(int index, T item) {
-        throw new NotYetImplementedException();
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        // handle the edge case of changing first element.
+        if (index == 0) {
+            Node<T> temp = new Node<T>(null, item, this.front.next);
+            if (this.front.next != null) {
+                this.front.next.prev = temp;
+            }
+            this.front = temp;
+            // change back pointer if only one element in whole list.
+            if (size == 1) {
+                this.back = this.front;
+            }
+        } else {
+            // quickly get the node at the given index.
+            Node<T> temp = getNode(index);
+            // shove a new point with the requested data and change links.
+            temp = new Node<T>(temp.prev, item, temp.next);
+            temp.prev.next = temp;
+            // update the back pointer if last element was changed.
+            if (temp.next != null) {
+                temp.next.prev = temp;
+            } else {
+                this.back = temp;
+            }
+        }
+    }
+
+    /**
+     * This methods helps in finding node at given index efficiently by using the least path to index from front or
+     * back.
+     * 
+     * @param index
+     *            the position in the list for which the node is to be found.
+     * @return the node at the given index
+     */
+    private Node<T> getNode(int index) {
+        // choose the most closest start point i.e. back or front.
+        // go front if difference between the size and index is
+        // larger than the half of the list.
+        Node<T> temp = this.front;
+        if (this.size - index > this.size / 2) {
+            int counter = 0;
+            while (counter != index && temp != null) {
+                temp = temp.next;
+                counter++;
+            }
+        } else {
+            temp = this.back;
+            int counter = this.size - 1;
+            while (counter != index && temp != null) {
+                temp = temp.prev;
+                counter--;
+            }
+        }
+        return temp;
     }
 
     @Override
     public void insert(int index, T item) {
-        throw new NotYetImplementedException();
+        if (index < 0 || index >= this.size + 1) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == this.size) {
+            this.add(item);
+        } else if (index == 0) {
+            Node<T> temp = new Node<T>(null, item, this.front);
+            this.front.prev = temp;
+            this.front = temp;
+            this.size++;
+            // change back pointer if only one element in whole list.
+            if (size == 1) {
+                this.back = this.front;
+            }
+        } else {
+            // quickly get the node at the given index.
+            Node<T> temp = getNode(index);
+            // shove a new point with the requested data and change links.
+            temp = new Node<T>(temp.prev, item, temp);
+            temp.prev.next = temp;
+            temp.next.prev = temp;
+            this.size++;
+        }
     }
 
     @Override
     public T delete(int index) {
-        throw new NotYetImplementedException();
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (this.size == 1 && index == 0) {
+            return this.remove();
+        }
+        T data;
+        if (index == 0) {
+            data = this.front.data;
+            this.front = this.front.next;
+            this.front.prev = null;
+        } else {
+            Node<T> temp = getNode(index);
+            data = temp.data;
+            // update back pointer for last element.
+            if (temp.next == null) {
+                this.back = temp.prev;
+                this.back.next = null;
+            } else {
+                temp.prev.next = temp.next;
+                temp.next.prev = temp.prev;
+            }
+        }
+        size--;
+        return data;
     }
 
     @Override
     public int indexOf(T item) {
-        throw new NotYetImplementedException();
+        Node<T> temp = this.front;
+        int index = 0;
+        while (temp != null) {
+            if ((item == null && temp.data == item) || temp.data.equals(item)) {
+                return index;
+            }
+            temp = temp.next;
+            index++;
+        }
+        return -1;
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return this.size;
     }
 
     @Override
     public boolean contains(T other) {
-        throw new NotYetImplementedException();
+        if (this.indexOf(other) > -1) {
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -108,22 +248,28 @@ public class DoubleLinkedList<T> implements IList<T> {
         }
 
         /**
-         * Returns 'true' if the iterator still has elements to look at;
-         * returns 'false' otherwise.
+         * Returns 'true' if the iterator still has elements to look at; returns 'false' otherwise.
          */
         public boolean hasNext() {
-            throw new NotYetImplementedException();
+            if (current != null) {
+                return true;
+            }
+            return false;
         }
 
         /**
-         * Returns the next item in the iteration and internally updates the
-         * iterator to advance one element forward.
+         * Returns the next item in the iteration and internally updates the iterator to advance one element forward.
          *
-         * @throws NoSuchElementException if we have reached the end of the iteration and
-         *         there are no more elements to look at.
+         * @throws NoSuchElementException
+         *             if we have reached the end of the iteration and there are no more elements to look at.
          */
         public T next() {
-            throw new NotYetImplementedException();
+            if (!this.hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T ans = current.data;
+            current = current.next;
+            return ans;
         }
     }
 }
