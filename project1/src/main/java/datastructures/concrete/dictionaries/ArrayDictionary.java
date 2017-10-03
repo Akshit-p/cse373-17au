0032b789ab7c9a6c2fa92a9f1c5023ebcf122b5f
Akshit Patel <akshitpatel.cs@gmail.com>
2017-10-03 00:14:40 -0700

    implemented fully

--- project1/src/main/java/datastructures/concrete/dictionaries/ArrayDictionary.java
+++ project1/src/main/java/datastructures/concrete/dictionaries/ArrayDictionary.java
@@ -11,11 +11,13 @@ public class ArrayDictionary<K, V> implements IDictionary<K, V> {
     // You may not change or rename this field: we will be inspecting
     // it using our private tests.
     private Pair<K, V>[] pairs;
-
-    // You're encouraged to add extra fields (and helper methods) though!
-
+    
+    // number of elements in the dictionary.
+    private int size = 0;
+    
+    
     public ArrayDictionary() {
-        throw new UnsupportedOperationException();
+        this.pairs = makeArrayOfPairs(10);
     }
 
     /**
@@ -42,27 +44,96 @@ public class ArrayDictionary<K, V> implements IDictionary<K, V> {
 
     @Override
     public V get(K key) {
-        throw new NotYetImplementedException();
+        int index = getIndexOf(key);
+        if (index < 0){
+            throw new NoSuchKeyException();
+        }
+        return this.pairs[index].value;
+        
     }
 
     @Override
     public void put(K key, V value) {
-        throw new NotYetImplementedException();
+        // grow the array if full else add the pair or update.
+        if (this.size == this.pairs.length) {
+            grow();
+        }
+        
+        int index = getIndexOf(key);
+        // if new key found then add the new pair.
+        if (index == -1) {
+            this.pairs[this.size] = new Pair<K, V>(key,value);
+            this.size++;
+        } else {    
+            this.pairs[index].value = value;
+        }
+    }
+    
+    /**
+     * Expands the size of the array to be twice original.
+     */
+    private void grow() {
+        int length = this.pairs.length;
+        // double the size for new array
+        Pair<K,V>[] copy = makeArrayOfPairs(length*2);
+        for (int i = 0; i < length; i++) {
+            copy[i] = new Pair<K,V>(this.pairs[i]);
+        }
+        this.pairs = copy;
+    }
+    
+    
+    
+    /**
+     * Returns the index of the key to be found in this.
+     * 
+     * @param key the key to be searched in this.
+     * @return the index of the key if found, otherwise -1.
+     */
+    private int getIndexOf(K key) {
+        // loop until we have elements and get the index
+        for (int i = 0; i < this.size; i++) {
+            K checkKey = this.pairs[i].key;
+            // check for null keys too.
+            if((key != null && checkKey.equals(key)) || key == checkKey) {
+                return i;
+            }
+        }
+        
+        // no index for given key found
+        return -1;
     }
 
     @Override
     public V remove(K key) {
-        throw new NotYetImplementedException();
+        int index = getIndexOf(key);
+        if (index < 0) {
+            throw new NoSuchKeyException();
+        }
+        V value = this.pairs[index].value;
+        shift(index);
+        size--;
+        return value;
+    }
+
+    private void shift(int index) {
+        if (this.size > 0) {
+            System.arraycopy(this.pairs, index + 1, this.pairs, index, size - index - 1);
+        }
+        
     }
 
     @Override
     public boolean containsKey(K key) {
-        throw new NotYetImplementedException();
+        if (getIndexOf(key) >= 0) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return this.size;
     }
 
     private static class Pair<K, V> {
@@ -73,6 +144,17 @@ public class ArrayDictionary<K, V> implements IDictionary<K, V> {
         public Pair(K key, V value) {
             this.key = key;
             this.value = value;
+        }
+        
+        /**
+         * Copy constructor to create new instance of 
+         * pair form other Pair instance.
+         * 
+         * @param other the Pair object from which to create a new instance
+         */
+        public Pair(Pair<K, V> other) {
+            this.key = other.key;
+            this.value = other.value;
         }
 
         @Override
