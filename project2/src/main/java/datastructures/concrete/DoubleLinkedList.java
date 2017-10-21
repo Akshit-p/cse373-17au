6e5fee93b8fcc8e8f4e921af884f9b8b00bab4db
Akshit Patel <akshitpatel.cs@gmail.com>
2017-10-20 23:33:00 -0700

    Project 2 setup

---
+++ project2/src/main/java/datastructures/concrete/DoubleLinkedList.java
@@ -0,0 +1,280 @@
+package datastructures.concrete;
+
+import datastructures.interfaces.IList;
+import misc.exceptions.EmptyContainerException;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Note: For more info on the expected behavior of your methods, see the source code for IList.
+ * 
+ * @author Akshit Patel
+ * @author Haobo Zhang
+ */
+public class DoubleLinkedList<T> implements IList<T> {
+    // You may not rename these fields or change their types.
+    // We will be inspecting these in our private tests.
+    // You also may not add any additional fields.
+    private Node<T> front;
+    private Node<T> back;
+    private int size;
+
+    public DoubleLinkedList() {
+        this.front = null;
+        this.back = null;
+        this.size = 0;
+    }
+
+    @Override
+    public void add(T item) {
+        if (this.size == 0) {
+            this.front = new Node<T>(item);
+            this.back = this.front;
+        } else {
+            this.back = new Node<T>(this.back, item, null);
+            this.back.prev.next = this.back;
+        }
+        this.size++;
+    }
+
+    @Override
+    public T remove() {
+        if (this.isEmpty()) {
+            throw new EmptyContainerException();
+        }
+        T data;
+        if (this.size == 1) {
+            data = this.front.data;
+            this.front = null;
+            this.back = null;
+        } else {
+            data = this.back.data;
+            this.back = this.back.prev;
+            this.back.next = null;
+        }
+        size--;
+        return data;
+    }
+
+    @Override
+    public T get(int index) {
+        indexBoundCheck(index);
+        return getNode(index).data;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        indexBoundCheck(index);
+
+        // handle the edge case of changing first element.
+        if (index == 0) {
+            Node<T> temp = new Node<T>(null, item, this.front.next);
+            // update the prev counter of the second element in list if present
+            if (this.front.next != null) {
+                this.front.next.prev = temp;
+            }
+            this.front = temp;
+            // change back pointer if only one element in whole list.
+            if (size == 1) {
+                this.back = this.front;
+            }
+        } else {
+            // quickly get the node at the given index.
+            Node<T> temp = getNode(index);
+            // shove a new point with the requested data and change links.
+            temp = new Node<T>(temp.prev, item, temp.next);
+            temp.prev.next = temp;
+            // update the back pointer if last element was changed.
+            if (temp.next != null) {
+                temp.next.prev = temp;
+            } else {
+                this.back = temp;
+            }
+        }
+    }
+
+    /**
+     * This methods helps in finding node at given index efficiently by using the 
+     * least path to index from front or back.
+     * 
+     * @param index
+     *            the position in the list for which the node is to be found.
+     * @return the node at the given index
+     */
+    private Node<T> getNode(int index) {
+        // choose the most closest start point i.e. back or front.
+        // go front if difference between the size and index is
+        // larger than the half of the list.
+        Node<T> temp = this.front;
+        if (this.size - index > this.size / 2) {
+            int counter = 0;
+            while (counter != index && temp != null) {
+                temp = temp.next;
+                counter++;
+            }
+        } else {
+            temp = this.back;
+            int counter = this.size - 1;
+            while (counter != index && temp != null) {
+                temp = temp.prev;
+                counter--;
+            }
+        }
+        return temp;
+    }
+
+    @Override
+    public void insert(int index, T item) {
+        if (index < 0 || index >= this.size + 1) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == this.size) {
+            this.add(item);
+        } else if (index == 0) {
+            Node<T> temp = new Node<T>(null, item, this.front);
+            this.front.prev = temp;
+            this.front = temp;
+            this.size++;
+        } else {
+            // quickly get the node at the given index.
+            Node<T> temp = getNode(index);
+            // shove a new point with the requested data and change links.
+            temp = new Node<T>(temp.prev, item, temp);
+            temp.prev.next = temp;
+            temp.next.prev = temp;
+            this.size++;
+        }
+    }
+
+    @Override
+    public T delete(int index) {
+        indexBoundCheck(index);
+
+        if (this.size == 1 && index == 0) {
+            return this.remove();
+        }
+        
+        T data;
+        if (index == 0) {
+            data = this.front.data;
+            this.front = this.front.next;
+            this.front.prev = null;
+        } else {
+            Node<T> temp = getNode(index);
+            data = temp.data;
+            
+            // update back pointer for last element.
+            if (temp.next == null) {
+                this.back = temp.prev;
+                this.back.next = null;
+            } else {
+                temp.prev.next = temp.next;
+                temp.next.prev = temp.prev;
+            }
+        }
+        size--;
+        return data;
+    }
+
+    @Override
+    public int indexOf(T item) {
+        Node<T> temp = this.front;
+        int index = 0;
+        while (temp != null) {
+            if ((item == null && temp.data == item) || temp.data.equals(item)) {
+                return index;
+            }
+            temp = temp.next;
+            index++;
+        }
+        return -1;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean contains(T other) {
+        if (this.indexOf(other) > -1) {
+            return true;
+        }
+        return false;
+    }
+    
+    /** 
+     * helper method to check for index out of bounds exception.
+     * @param index
+     */
+    private void indexBoundCheck(int index) {
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        // Note: we have provided a part of the implementation of
+        // an iterator for you. You should complete the methods stubs
+        // in the DoubleLinkedListIterator inner class at the bottom
+        // of this file. You do not need to change this method.
+        return new DoubleLinkedListIterator<>(this.front);
+    }
+
+    private static class Node<E> {
+        // You may not change the fields in this node or add any new fields.
+        public final E data;
+        public Node<E> prev;
+        public Node<E> next;
+
+        public Node(Node<E> prev, E data, Node<E> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public Node(E data) {
+            this(null, data, null);
+        }
+
+        // Feel free to add additional constructors or methods to this class.
+    }
+
+    private static class DoubleLinkedListIterator<T> implements Iterator<T> {
+        // You should not need to change this field, or add any new fields.
+        private Node<T> current;
+
+        public DoubleLinkedListIterator(Node<T> current) {
+            // You do not need to make any changes to this constructor.
+            this.current = current;
+        }
+
+        /**
+         * Returns 'true' if the iterator still has elements to look at; returns 'false' otherwise.
+         */
+        public boolean hasNext() {
+            if (current != null) {
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * Returns the next item in the iteration and internally updates the iterator to advance one element forward.
+         *
+         * @throws NoSuchElementException
+         *             if we have reached the end of the iteration and there are no more elements to look at.
+         */
+        public T next() {
+            if (!this.hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T ans = current.data;
+            current = current.next;
+            return ans;
+        }
+    }
+}
---
+++ project2/src/main/java/datastructures/concrete/dictionaries/ArrayDictionary.java
@@ -0,0 +1,159 @@
+package datastructures.concrete.dictionaries;
+
+import datastructures.concrete.KVPair;
+import datastructures.interfaces.IDictionary;
+import misc.exceptions.NoSuchKeyException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * See IDictionary for more details on what this class should do
+ * 
+ * @author Akshit Patel
+ * @author Haobo Zhang
+ */
+public class ArrayDictionary<K, V> implements IDictionary<K, V> {
+    // You may not change or rename this field: we will be inspecting
+    // it using our private tests.
+    private Pair<K, V>[] pairs;
+
+    // number of elements in the dictionary.
+    private int size = 0;
+
+    public ArrayDictionary() {
+        this.pairs = makeArrayOfPairs(10);
+    }
+
+    /**
+     * This method will return a new, empty array of the given size that can contain Pair<K, V> objects.
+     *
+     * Note that each element in the array will initially be null.
+     */
+    @SuppressWarnings("unchecked")
+    private Pair<K, V>[] makeArrayOfPairs(int arraySize) {
+        // It turns out that creating arrays of generic objects in Java
+        // is complicated due to something known as 'type erasure'.
+        //
+        // We've given you this helper method to help simplify this part of
+        // your assignment. Use this helper method as appropriate when
+        // implementing the rest of this class.
+        //
+        // You are not required to understand how this method works, what
+        // type erasure is, or how arrays and generics interact. Do not
+        // modify this method in any way.
+        return (Pair<K, V>[]) (new Pair[arraySize]);
+
+    }
+
+    @Override
+    public V get(K key) {
+        int index = getIndexOf(key);
+        if (index < 0) {
+            throw new NoSuchKeyException();
+        }
+        return this.pairs[index].value;
+
+    }
+
+    @Override
+    public void put(K key, V value) {
+        // grow the array if full else add the pair or update.
+        if (this.size == this.pairs.length) {
+            grow();
+        }
+
+        int index = getIndexOf(key);
+        // if new key found then add the new pair.
+        if (index == -1) {
+            this.pairs[this.size] = new Pair<K, V>(key, value);
+            this.size++;
+        } else {
+            this.pairs[index].value = value;
+        }
+    }
+
+    /**
+     * Expands the size of the array to be twice original.
+     */
+    private void grow() {
+        int length = this.pairs.length;
+        // double the size for new array
+        Pair<K, V>[] copy = makeArrayOfPairs(length * 2);
+        System.arraycopy(this.pairs, 0, copy, 0, this.size);
+        this.pairs = copy;
+    }
+
+    /**
+     * Returns the index of the key to be found in this.
+     * 
+     * @param key
+     *            the key to be searched in this.
+     * @return the index of the key if found, otherwise -1.
+     */
+    private int getIndexOf(K key) {
+        // loop until we have elements and get the index
+        for (int i = 0; i < this.size; i++) {
+            K checkKey = this.pairs[i].key;
+            // check for null keys too.
+            if ((key != null && checkKey.equals(key)) || key == checkKey) {
+                return i;
+            }
+        }
+
+        // no index for given key found
+        return -1;
+    }
+
+    @Override
+    public V remove(K key) {
+        int index = getIndexOf(key);
+        if (index < 0) {
+            throw new NoSuchKeyException();
+        }
+        V value = this.pairs[index].value;
+        shift(index);
+        size--;
+        return value;
+    }
+
+    private void shift(int index) {
+        if (this.size > 0) {
+            System.arraycopy(this.pairs, index + 1, this.pairs, index, size - index - 1);
+        }
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        if (getIndexOf(key) >= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    private static class Pair<K, V> {
+        public K key;
+        public V value;
+
+        // You may add constructors and methods to this class as necessary.
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return this.key + "=" + this.value;
+        }
+    }
+
+    @Override
+    public Iterator<KVPair<K, V>> iterator() {
+        throw new NoSuchElementException();
+    }
+}
