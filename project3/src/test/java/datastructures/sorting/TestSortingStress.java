52966b399f55f852d40d566d6b85b5bc4b8ab75c
Akshit Patel <akshitpatel.cs@gmail.com>
2017-11-13 15:04:41 -0800

    arrayheap stress test added

--- project3/src/test/java/datastructures/sorting/TestSortingStress.java
+++ project3/src/test/java/datastructures/sorting/TestSortingStress.java
@@ -3,15 +3,44 @@ package datastructures.sorting;
 import misc.BaseTest;
 import org.junit.Test;
 
+import datastructures.concrete.ArrayHeap;
+import datastructures.interfaces.IPriorityQueue;
+
 import static org.junit.Assert.assertTrue;
 
 /**
  * See spec for details on what kinds of tests this class should include.
  */
 public class TestSortingStress extends BaseTest {
+    
     @Test(timeout=10*SECOND)
-    public void testPlaceholder() {
-        // TODO: replace this placeholder with actual code
-        assertTrue(true);
+    public void testArrayHeapStress() {
+        int limit = 1000000;
+        IPriorityQueue<Integer> heap = makeInstance();
+        for (int i = 0; i < limit; i++) {
+            heap.insert(i);
+            assertEquals(0,heap.peekMin());
+            assertEquals(i+1, heap.size());
+        }
+        
+        for (int i = 0; i < limit; i++) {
+            int min = heap.peekMin();
+            assertEquals(min, heap.removeMin());
+        }
+        assertTrue(heap.isEmpty());
+        
+        for (int i = 0; i < limit; i++) {
+            heap.insert(i);
+            assertEquals(i,heap.peekMin());
+            assertEquals(1, heap.size());
+            int min = heap.peekMin();
+            assertEquals(min, heap.removeMin());
+        }
+        assertTrue(heap.isEmpty());
+        
+    }
+
+    protected <T extends Comparable<T>> IPriorityQueue<T> makeInstance() {
+        return new ArrayHeap<>();
     }
 }
