ec380b19017fd5fe0b668714fbc4f387f77add90
abc <abc@d-10-157-41-106.dhcp4.washington.edu>
2017-11-14 00:35:42 -0800

    testing for topK

--- project3/src/test/java/datastructures/sorting/TestSortingStress.java
+++ project3/src/test/java/datastructures/sorting/TestSortingStress.java
@@ -1,12 +1,20 @@
 package datastructures.sorting;
 
 import misc.BaseTest;
+import misc.Searcher;
+
 import org.junit.Test;
 
 import datastructures.concrete.ArrayHeap;
+import datastructures.concrete.DoubleLinkedList;
+import datastructures.interfaces.IList;
 import datastructures.interfaces.IPriorityQueue;
 
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * See spec for details on what kinds of tests this class should include.
@@ -38,6 +46,39 @@ public class TestSortingStress extends BaseTest {
         }
         assertTrue(heap.isEmpty());
         
+    }
+    
+    @Test(timeout=10*SECOND)
+    public void testTopKStringListStress() {
+        List<String> sortList = new ArrayList<>(1000000);
+        
+        IList<String> list = new DoubleLinkedList<>();
+        for (int i = 0; i < 1000000; i++) {
+            list.add("a"+i);
+            sortList.add("a"+i);
+        }
+        Collections.sort(sortList);
+        IList<String> top = Searcher.topKSort(50000, list);
+        assertEquals(5, top.size());
+        for (int i = 950000; i < top.size(); i++) {
+            assertEquals(sortList.get(i), top.get(i));           
+        }
+    }
+    
+    @Test(timeout=10*SECOND)
+    public void testTopKIntegerListStress() {
+        List<Integer> sortList = new ArrayList<>(1000000);
+        IList<Integer> list = new DoubleLinkedList<>();
+        for (int i = 1000000; i > 0; i--) {
+            list.add(i);
+            sortList.add(i);
+        }
+        Collections.sort(sortList);
+        IList<Integer> top = Searcher.topKSort(50000, list);
+        assertEquals(5, top.size());
+        for (int i = 950000; i < top.size(); i++) {
+            assertEquals(sortList.get(i), top.get(i));           
+        }
     }
 
     protected <T extends Comparable<T>> IPriorityQueue<T> makeInstance() {
--- project3/src/test/java/datastructures/sorting/TestTopKSortFunctionality.java
+++ project3/src/test/java/datastructures/sorting/TestTopKSortFunctionality.java
@@ -4,6 +4,13 @@ import misc.BaseTest;
 import datastructures.concrete.DoubleLinkedList;
 import datastructures.interfaces.IList;
 import misc.Searcher;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Test;
 
 /**
@@ -16,11 +23,97 @@ public class TestTopKSortFunctionality extends BaseTest {
         for (int i = 0; i < 20; i++) {
             list.add(i);
         }
-
         IList<Integer> top = Searcher.topKSort(5, list);
         assertEquals(5, top.size());
         for (int i = 0; i < top.size(); i++) {
             assertEquals(15 + i, top.get(i));
+        }
+    }
+    
+    @Test(timeout=SECOND, expected=IllegalArgumentException.class)
+    public void testException() {
+        Searcher.topKSort(-1, new DoubleLinkedList<Integer>());
+    }
+    
+    @Test(timeout=SECOND)
+    public void testKLargerThanSize() {
+        IList<Integer> list = new DoubleLinkedList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(i);
+        }
+        IList<Integer> top = Searcher.topKSort(101, list);
+        assertEquals(100, top.size());
+        for (int i = 0; i < top.size(); i++) {
+            assertEquals(i, top.get(i));
+        }
+    }
+    
+    @Test(timeout=SECOND)
+    public void testKEqualsSize() {
+        IList<Integer> list = new DoubleLinkedList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(i);
+        }
+        IList<Integer> top = Searcher.topKSort(100, list);
+        assertEquals(100, top.size());
+        for (int i = 0; i < top.size(); i++) {
+            assertEquals(i, top.get(i));
+        }
+    }
+    
+    @Test(timeout=SECOND)
+    public void testKEqualsZero() {
+        IList<Integer> top = Searcher.topKSort(0, new DoubleLinkedList<Integer>());
+        assertTrue(top.isEmpty());
+    }
+    
+    @Test(timeout=SECOND)
+    public void testUnmodifiedList() {
+        IList<Integer> list = new DoubleLinkedList<>();
+        for (int i = 20; i > 0; i--) {
+            list.add(i);
+        }
+        IList<Integer> top = Searcher.topKSort(0, list);
+        assertTrue(top.isEmpty());
+        top = Searcher.topKSort(19, list);
+        assertEquals(19, top.size());
+        for (int i = 2; i < top.size(); i++) {
+            assertEquals(i, top.get(i));
+        }
+        //Check for no modification for the client list
+        for (int i = 0; i < list.size(); i++) {
+            assertEquals(20 - i, list.get(i));
+        }
+    }
+    
+    @Test(timeout=SECOND)
+    public void testStringListSimple() {
+        IList<String> list = new DoubleLinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add("a"+i);
+        }
+        IList<String> top = Searcher.topKSort(5, list);
+        assertEquals(5, top.size());
+        for (int i = 5; i < top.size(); i++) {
+            assertEquals("a" + i, top.get(i));
+        }
+    }
+    
+    
+    @Test(timeout=SECOND)
+    public void testStringListComplex() {
+        List<String> sortList = new ArrayList<>(500);
+        
+        IList<String> list = new DoubleLinkedList<>();
+        for (int i = 0; i < 500; i++) {
+            list.add("a"+i);
+            sortList.add("a"+i);
+        }
+        Collections.sort(sortList);
+        IList<String> top = Searcher.topKSort(200, list);
+        assertEquals(5, top.size());
+        for (int i = 300; i < top.size(); i++) {
+            assertEquals(sortList.get(i), top.get(i));           
         }
     }
 }
