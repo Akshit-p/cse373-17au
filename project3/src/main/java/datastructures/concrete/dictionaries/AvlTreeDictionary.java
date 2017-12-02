a5278bf5a0444d7c10bbc07d5249a6a2acef1fad
Akshit Patel <akshitpatel.cs@gmail.com>
2017-12-01 17:55:59 -0800

    debuged

--- project3/src/main/java/datastructures/concrete/dictionaries/AvlTreeDictionary.java
+++ project3/src/main/java/datastructures/concrete/dictionaries/AvlTreeDictionary.java
@@ -7,77 +7,78 @@ import datastructures.interfaces.IDictionary;
 import misc.exceptions.NoSuchKeyException;
 
 public class AvlTreeDictionary<K extends Comparable<K>, V> implements IDictionary<K, V> {
-    
+
     private node root;
     private int size;
-    
+
     private class node {
-        private KVPair<K,V> data;
+        private KVPair<K, V> data;
         private node left;
         private node right;
         private int height;
-        
+
         public node(K key, V value) {
-            this.data = new KVPair<K,V>(key, value);
+            this.data = new KVPair<K, V>(key, value);
             this.left = null;
             this.right = null;
             this.height = 0;
         }
-        
+
         public node(K key, V value, node left, node right) {
-        		this.data = new KVPair<K,V>(key, value);
+            this.data = new KVPair<K, V>(key, value);
             this.left = left;
             this.right = right;
             this.height = left.height + right.height;
         }
     }
-    
+
     public AvlTreeDictionary() {
         this.root = null;
         this.size = 0;
     }
-    
+
     @Override
     public V get(K key) {
         return get(key, this.root);
     }
 
     private V get(K key, node current) {
-        if (current != null) {
-            if (current.data.getKey().equals(key)) {
-                return current.data.getValue();
-            }
-            if (current.data.getKey().compareTo(key) > 0) { 
-                get(key, current.left);
+        if (current == null) {
+            throw new NoSuchKeyException();
+        }
+        if (current.data.getKey().compareTo(key) == 0) {
+            return current.data.getValue();
+        } else {
+            if (current.data.getKey().compareTo(key) > 0) {
+                return get(key, current.left);
             } else {
-                get(key, current.right);
+                return get(key, current.right);
             }
         }
-        throw new NoSuchKeyException();
     }
 
     @Override
     public void put(K key, V value) {
-        	this.root = put(key, value, this.root);
+        this.root = put(key, value, this.root);
     }
-    
-    
-    private node put(K key, V value, node current) {
-		if(current != null) {
-			if (current.data.getKey().compareTo(key) == 0) {
-				this.size++;
-				return new node(key, value, current.left, current.right);
-			}
-			if (key.compareTo(current.data.getKey())<0) {
-				current.left = put(key, value, current.left);
-			}else {
-				current.right = put(key, value, current.right);
-			}
-		}
-		return new node(key, value);
-	}
 
-	@Override
+    private node put(K key, V value, node current) {
+        if (current == null) {
+            return new node(key, value);
+        }
+        if (current.data.getKey().compareTo(key) == 0) {
+            return new node(key, value);
+        }
+        if (key.compareTo(current.data.getKey()) < 0) {
+            current.left = put(key, value, current.left);
+        } else {
+            current.right = put(key, value, current.right);
+        }
+        this.size++;
+        return current;
+    }
+
+    @Override
     public V remove(K key) {
         // TODO Auto-generated method stub
         return null;
@@ -85,10 +86,10 @@ public class AvlTreeDictionary<K extends Comparable<K>, V> implements IDictionar
 
     @Override
     public boolean containsKey(K key) {
-        try{
-            this.get(key,this.root);
+        try {
+            this.get(key, this.root);
             return true;
-        } catch(NoSuchKeyException e) {
+        } catch (NoSuchKeyException e) {
             return false;
         }
     }
@@ -103,8 +104,8 @@ public class AvlTreeDictionary<K extends Comparable<K>, V> implements IDictionar
         // TODO Auto-generated method stub
         return null;
     }
-    
+
     private node rotate() {
-    		return null;
+        return null;
     }
 }
